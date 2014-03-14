056a57cee33ed9681f9649af3da5ef13e34ea61c
michael lechner <mlechner2@gmail.com>
2014-03-14 10:21:16 -0600

    Misc development work

---
+++ refactoru-python-example/hello.py
@@ -0,0 +1,26 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Booleans: 
+    True
+    False
+Operators:
+    and
+    or
+    not
+Equality
+    ==
+    is (===)
+Falseys
+    None
+Compares by value and not by reference
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+# comment
+x = 7
+y = 10
+
+if x > 5 and y == 10:
+    print('hello')
+elif x < 3:
+    print('So tiny!')
+else:
+    print('Not so much')
